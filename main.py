bf7055293d642f5456e00604bc9dfde699b6383c
ParivalavanIT <112223851+ParivalavanIT@users.noreply.github.com>
2023-11-22 16:38:27 +0530

    sync

--- main.py
+++ main.py
@@ -2,12 +2,13 @@ import re
 import pandas as pd
 import pyttsx3
 from sklearn import preprocessing
-from sklearn.tree import DecisionTreeClassifier,_tree
+from sklearn.tree import DecisionTreeClassifier, _tree
 import numpy as np
 from sklearn.model_selection import train_test_split
 from sklearn.model_selection import cross_val_score
 from sklearn.svm import SVC
 import csv
+import json
 import warnings
 import streamlit as st
 from streamlit_lottie import st_lottie
@@ -26,49 +27,51 @@ def get(path: str):
 
 
 home_path = get('./home.json')
-login_path = get('./login.json')
-success_path = get('./success.json')
+# login_path = get('./login.json')
+# success_path = get('./success.json')
 
 training = pd.read_csv('Data/Training.csv')
-testing= pd.read_csv('Data/Testing.csv')
-cols= training.columns
-cols= cols[:-1]
+testing = pd.read_csv('Data/Testing.csv')
+cols = training.columns
+cols = cols[:-1]
 x = training[cols]
 y = training['prognosis']
-y1= y
+y1 = y
 
 
 reduced_data = training.groupby(training['prognosis']).max()
 
-#mapping strings to numbers
+# mapping strings to numbers
 le = preprocessing.LabelEncoder()
 le.fit(y)
 y = le.transform(y)
 
 
-x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.33, random_state=42)
-testx    = testing[cols]
-testy    = testing['prognosis']
-testy    = le.transform(testy)
+x_train, x_test, y_train, y_test = train_test_split(
+    x, y, test_size=0.33, random_state=42)
+testx = testing[cols]
+testy = testing['prognosis']
+testy = le.transform(testy)
 
 
-clf1  = DecisionTreeClassifier()
-clf = clf1.fit(x_train,y_train)
+clf1 = DecisionTreeClassifier()
+clf = clf1.fit(x_train, y_train)
 # print(clf.score(x_train,y_train))
 # print ("cross result========")
 scores = cross_val_score(clf, x_test, y_test, cv=3)
 # print (scores)
-print (scores.mean())
+print(scores.mean())
 
 
-model=SVC()
-model.fit(x_train,y_train)
+model = SVC()
+model.fit(x_train, y_train)
 print("for svm: ")
-print(model.score(x_test,y_test))
+print(model.score(x_test, y_test))
 
 importances = clf.feature_importances_
 indices = np.argsort(importances)[::-1]
 features = cols
+
 
 def readn(nstr):
     engine = pyttsx3.init()
@@ -81,14 +84,14 @@ def readn(nstr):
     engine.stop()
 
 
-severityDictionary=dict()
+severityDictionary = dict()
 description_list = dict()
-precautionDictionary=dict()
+precautionDictionary = dict()
 
 symptoms_dict = {}
 
 for index, symptom in enumerate(x):
-       symptoms_dict[symptom] = index
+    symptoms_dict[symptom] = index
 
 
 def calc_condition(exp, days, severity_dict):
@@ -117,6 +120,8 @@ def getSeverityDict():
     return severityDictionary
 
 # Function to load description data from CSV
+
+
 def getDescription():
     description_list = {}
     with open('MasterData/symptom_Description.csv') as csv_file:
@@ -126,6 +131,8 @@ def getDescription():
     return description_list
 
 # Function to load precaution data from CSV
+
+
 def getprecautionDict():
     precautionDictionary = {}
     with open('MasterData/symptom_precaution.csv') as csv_file:
@@ -135,6 +142,8 @@ def getprecautionDict():
     return precautionDictionary
 
 # Function to get user information using Streamlit
+
+
 def getInfo():
     st.title("HealthCare ChatBot")
     name = st.text_input("Your Name?")
@@ -154,11 +163,13 @@ def check_pattern(dis_list, inp):
     else:
         return False, []
 
+
 def sec_predict(symptoms_exp):
     df = pd.read_csv('Data/Training.csv')
     X = df.iloc[:, :-1]
     y = df['prognosis']
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=20)
+    X_train, X_test, y_train, y_test = train_test_split(
+        X, y, test_size=0.3, random_state=20)
     rf_clf = DecisionTreeClassifier()
     rf_clf.fit(X_train, y_train)
 
@@ -171,12 +182,11 @@ def sec_predict(symptoms_exp):
     return rf_clf.predict([input_vector])
 
 
-
 def print_disease(node):
     node = node[0]
-    val  = node.nonzero()
+    val = node.nonzero()
     disease = le.inverse_transform(val[0])
-    return list(map(lambda x:x.strip(),list(disease)))
+    return list(map(lambda x: x.strip(), list(disease)))
 
 
 def tree_diagnosis(clf, feature_names, user_symptoms, num_days, severity_dict, description_dict, precaution_dict):
@@ -201,22 +211,26 @@ def tree_diagnosis(clf, feature_names, user_symptoms, num_days, severity_dict, d
                 val = 0
 
             if val <= threshold:
-                recurse(tree_.children_left[node], depth + 1, user_symptoms, num_days)
+                recurse(tree_.children_left[node],
+                        depth + 1, user_symptoms, num_days)
             else:
                 symptoms_present.append(name)
-                recurse(tree_.children_right[node], depth + 1, user_symptoms, num_days)
+                recurse(tree_.children_right[node],
+                        depth + 1, user_symptoms, num_days)
         else:
             present_disease = print_disease(tree_.value[node])
             symptoms_exp = []  # Use Streamlit input elements to get symptoms
             second_prediction = sec_predict(symptoms_exp)
-            condition_result = calc_condition(symptoms_exp, num_days, severity_dict)
+            condition_result = calc_condition(
+                symptoms_exp, num_days, severity_dict)
 
             # Display diagnosis info using Streamlit interface
             if present_disease[0] == second_prediction[0]:
                 st.write("You may have ", present_disease[0])
                 st.write(description_dict[present_disease[0]])
             else:
-                st.write("You may have ", present_disease[0], "or ", second_prediction[0])
+                st.write("You may have ",
+                         present_disease[0], "or ", second_prediction[0])
                 st.write(description_dict[present_disease[0]])
                 st.write(description_dict[second_prediction[0]])
 
@@ -227,6 +241,7 @@ def tree_diagnosis(clf, feature_names, user_symptoms, num_days, severity_dict, d
 
     recurse(0, 1, user_symptoms, num_days)
 
+
 # Load data and necessary information
 severityDictionary = getSeverityDict()
 description_list = getDescription()
@@ -235,17 +250,20 @@ precautionDictionary = getprecautionDict()
 clf = DecisionTreeClassifier()  # You need to load your trained classifier here
 
 # Fit your classifier with training data
-cols= training.columns
-cols= cols[:-1]
+cols = training.columns
+cols = cols[:-1]
 x = training[cols]
 y = training['prognosis']
 le = preprocessing.LabelEncoder()
 le.fit(y)
 y = le.transform(y)
-x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.33, random_state=42)
-clf = clf.fit(x_train,y_train)
+x_train, x_test, y_train, y_test = train_test_split(
+    x, y, test_size=0.33, random_state=42)
+clf = clf.fit(x_train, y_train)
 
 # Initiate Streamlit app
+
+
 def main():
     st.title("HealthCare ChatBot")
 
@@ -253,12 +271,12 @@ def main():
     choice = st.sidebar.selectbox("Menu", menu)
 
     if choice == "Home":
-        st.subheader("Home")
+        st_lottie(home_path)
         # Your home page content goes here
 
     elif choice == "Login":
         st.subheader("Login Section")
-
+        
         username = st.sidebar.text_input("User Name")
         password = st.sidebar.text_input("Password", type='password')
 
@@ -267,20 +285,25 @@ def main():
                 st.success("Logged In as {}".format(username))
 
                 # Your application code goes here
-                user_input = st.text_input("Enter your symptoms separated by comma")  # Get user symptoms
-                num_days = st.number_input("From how many days?", min_value=1, key='num_days')  # Get number of days
+                user_input = st.text_input(
+                    "Enter your symptoms separated by comma")  # Get user symptoms
+                num_days = st.number_input(
+                    "From how many days?", min_value=1, key='num_days')  # Get number of days
 
                 if st.button("Diagnose"):  # Button to trigger diagnosis
-                    user_symptoms = user_input.split(",")  # Split user input into list of symptoms
+                    # Split user input into list of symptoms
+                    user_symptoms = user_input.split(",")
 
                     # Obtain feature names from the training dataset columns
-                    feature_names = list(training.columns[:-1])  # Assuming the last column is the target
+                    # Assuming the last column is the target
+                    feature_names = list(training.columns[:-1])
 
                     # Call tree_diagnosis with all the necessary parameters
                     tree_diagnosis(clf, feature_names, user_symptoms, num_days, severityDictionary, description_list,
-                                precautionDictionary)
+                                   precautionDictionary)
             else:
                 st.warning("Incorrect Username/Password")
+
 
 if __name__ == "__main__":
     main()
